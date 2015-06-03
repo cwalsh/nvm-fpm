f249d97381a36038d5dda7bd5a13857ee9fe25e0
Cameron Walsh <cameron.walsh@bigcommerce.com>
2015-06-03 14:00:01 +1000

    Use only the numeric portion of the tag to generate the .deb version

--- fpnvm.sh
+++ fpnvm.sh
@@ -5,8 +5,8 @@ CHECKOUT_DIR=nvm
 rm -rf $CHECKOUT_DIR
 git clone https://github.com/creationix/nvm.git $CHECKOUT_DIR
 cd nvm
-VERSION=`git describe --abbrev=0 --tags`
-git checkout master
-git reset --hard $VERSION
+TAG=`git describe --abbrev=0 --tags`
+VERSION=`echo $TAG |sed 's/^[^0-9]*//'`
+git reset --hard $TAG
 cd ..
-fpm -f -s dir -t deb -n nvm -v $VERSION -x $CHECKOUT_DIR/test -x $CHECKOUT_DIR/'*.md' -x $CHECKOUT_DIR/Makefile -x $CHECKOUT_DIR/.git -x $CHECKOUT_DIR/'.*' --prefix=$PREFIX nvm
+fpm -f -s dir -t deb -n nvm -v $VERSION -x $CHECKOUT_DIR/test -x $CHECKOUT_DIR/'*.md' -x $CHECKOUT_DIR/Makefile -x $CHECKOUT_DIR/'.*' --prefix=$PREFIX nvm
