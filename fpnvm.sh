d85f083747cba5cf2b2adc44f7e003bf30c276c9
Cameron Walsh <cameron.walsh@bigcommerce.com>
2015-07-07 15:32:05 +1000

    License and description

--- fpnvm.sh
+++ fpnvm.sh
@@ -2,6 +2,7 @@
 set -e
 PREFIX=/usr/local/
 CHECKOUT_DIR=nvm
+DESCRIPTION="Node Version Manager - allow multiple versions of NodeJS"
 rm -rf $CHECKOUT_DIR
 git clone https://github.com/creationix/nvm.git $CHECKOUT_DIR
 cd nvm
@@ -9,4 +10,4 @@ TAG=`git describe --abbrev=0 --tags`
 VERSION=`echo $TAG |sed 's/^[^0-9]*//'`
 git reset --hard $TAG
 cd ..
-fpm -f -s dir -t deb -n nvm -v $VERSION -x $CHECKOUT_DIR/test -x $CHECKOUT_DIR/'*.md' -x $CHECKOUT_DIR/Makefile -x $CHECKOUT_DIR/'.*' --prefix=$PREFIX nvm
+fpm -f -s dir -t deb -n nvm -v $VERSION -x $CHECKOUT_DIR/test -x $CHECKOUT_DIR/'*.md' -x $CHECKOUT_DIR/Makefile -x $CHECKOUT_DIR/'.*' --prefix=$PREFIX --description="${DESCRIPTION}" nvm
